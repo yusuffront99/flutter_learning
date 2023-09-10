c505c7ecbdde622376682d4234f26e5a1f5c56e2
unknown <you@example.com>
2023-09-10 15:16:43 +0800

    design payment app

---
+++ payment_app/lib/theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+
+TextStyle proTextStyle = GoogleFonts.poppins(
+  color: Color(0xff007DFF),
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+
+TextStyle subTitleTextStyle = GoogleFonts.poppins(
+    color: Color(0xff899788), fontWeight: FontWeight.w300, fontSize: 16);
+
+TextStyle planTextStyle = GoogleFonts.poppins(
+    color: whiteColor, fontWeight: FontWeight.w500, fontSize: 14);
+
+TextStyle descTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 12,
+);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 10,
+);
+
+TextStyle buttonTitleTextStyle = GoogleFonts.poppins(
+    color: Color(0xffFFFFFF), fontWeight: FontWeight.w500, fontSize: 16);
